e5f77b9a04e5146ccba0fd5efe9aacac323d6d12
TrustDev <67072920+TrustDev@users.noreply.github.com>
2022-01-21 21:20:32 +0300

    add globalCases card

---
+++ src/components/globalCases.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import PieChart from 'react-native-pie-chart';
+
+interface GlobalCasesProps {
+  death: number,
+  recoveris: number,
+  active: number,
+}
+
+export const GlobalCases = ({
+  death,
+  active,
+  recoveris,
+}: GlobalCasesProps) => {
+  const CaseItem = (props : {label: string, value: number, color: string}) => {
+    return (
+      <View style={styles.countryItem}>
+        <Text style={[styles.countryText, {color: props.color}]}>{props.label}</Text>
+        <Text style={[styles.caseText, {color: props.color}]}>{props.value}</Text>
+      </View>
+    )
+  }
+  const widthAndHeight = 160
+  const series = [death, recoveris, active]
+  const sliceColor = ['#F44336','#2196F3','#4CAF50']
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Globally Cases</Text>
+      <View style={styles.contentContainer}>
+        <View style={styles.countryContainer}>
+          <CaseItem label="Death" color={'#F44336'} value={death}/>
+          <CaseItem label="Recoveries" color={'#2196F3'} value={recoveris}/>
+          <CaseItem label="Active Case" color={'#4CAF50'} value={active}/>
+        </View>
+        <PieChart
+          widthAndHeight={widthAndHeight}
+          series={series}
+          sliceColor={sliceColor}
+        />
+      </View>
+    </View>
+  );
+};
+GlobalCases.defaultProps = {
+  death: 110,
+  recoveris: 50,
+  active: 20
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    justifyContent: 'space-around',
+    paddingVertical: 20,
+    paddingHorizontal: 25,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    borderColor: '#dadada',
+    borderWidth: 1,
+    shadowColor: '#333',
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+    elevation: 5,
+  },
+  label: {
+    fontSize: 23,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  contentContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  },
+  countryContainer: {
+    alignContent: 'space-around',
+    justifyContent: 'space-between',
+  },
+  countryItem: {    
+    backgroundColor: 'white',
+    width: '100%',
+    marginBottom: 10,
+  },
+  countryText: {
+    fontFamily: 'Montserrat',
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  caseText: {
+    marginLeft: 10,
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+});
