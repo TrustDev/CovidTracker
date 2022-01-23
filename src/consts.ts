0ccd7d9d224a1edc75b693d17cf11ea5f9309f0a
TrustDev <67072920+TrustDev@users.noreply.github.com>
2022-01-23 20:14:28 +0300

    add api config

--- src/consts.ts
+++ src/consts.ts
@@ -6,3 +6,9 @@ export const Colors = {
   correct: '#00DEE9',
   wrong: '#FF7C89',
 };
+
+export const APIConfig = {
+  apiKey: '5cf9dfd5-3449-485e-b5ae-70a60e997864',
+  baseUrl: 'https://api.covid19api.com',
+  appId: 'CovidTracker'
+}
